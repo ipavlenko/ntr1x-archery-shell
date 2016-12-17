748c3cc8b1a5ce78ff3dcc3a7096f47106bf931d
Igor Pavlenko <ipavlenko@inbox.ru>
2016-12-17 15:44:17 +0500

    Sources editor

--- src/editor/storages/storages.js
+++ src/editor/storages/storages.js
@@ -2,7 +2,6 @@
 
     Shell.Storages = Shell.Storages || {};
 
-    var StoragesListViewer =
     Vue.component('storages-list', {
         template: '#storages-list',
         mixins: [Core.ListViewerMixin],
@@ -14,25 +13,21 @@
         mixins: [Core.ModalEditorMixin],
     });
 
-    var StoragesEditor =
     Vue.component('storages', {
         mixins: [Core.EditorMixin('storages-dialog')],
         template: '#storages',
     });
 
-    var StoragesVariablesListViewer =
     Vue.component('storages-variables-list', {
         template: '#storages-variables-list',
         mixins: [Core.ListViewerMixin],
     });
 
-    var StoragesVariablesModalEditor =
     Vue.component('storages-variables-dialog', {
         template: '#storages-variables-dialog',
         mixins: [Core.ModalEditorMixin],
     });
 
-    var StoragesVariablesEditor =
     Vue.component('storages-variables', {
         mixins: [Core.EditorMixin('storages-variables-dialog')],
         template: '#storages-variables',
--- src/shell/sources/sources.js
+++ src/shell/sources/sources.js
@@ -2,15 +2,10 @@
 
     Vue.component('shell-sources', {
         template: '#shell-sources',
-        props: {
-            sources: Array,
-            globals: Object,
-        },
         data: function() {
             return {
                 type: this.type,
                 types: this.types,
-                items: this.items,
             }
         },
         created: function() {
@@ -21,18 +16,6 @@
             ];
 
             this.type = this.types[0];
-
-            this.$on('sources/create', ({ value }) => {
-                this.$store.commit('designer/sources/create', value);
-            })
-
-            this.$on('sources/update', ({ value }) => {
-                this.$store.commit('designer/sources/update', value);
-            })
-
-            this.$on('sources/remove', ({ value }) => {
-                this.$store.commit('designer/sources/remove', value);
-            })
         },
         computed: {
             active: function() { return this.$store.state.designer.source; },
@@ -42,71 +25,39 @@
 
             remove: function(value) {
 
-                this.$emit('sources/remove', { value: value })
+                this.$store.commit('designer/sources/remove', value);
             },
 
             create: function() {
 
-                var source = {
-                    method: 'GET',
+                let source = {
                     type: this.type.name,
+                    method: 'GET',
                     params: [],
                     url: '',
                     name: ''
                 }
 
-                new Shell.Sources.ModalEditor({
-
-                    data: {
-                        globals: this.globals,
-                        owner: this,
-                        original: source,
-                        current: JSON.parse(JSON.stringify(source)),
-                    },
-
-                    methods: {
-                        submit: function() {
-
-                            this.owner.$emit('sources/create', {
-                                value: this.current
-                            });
-
-                            this.$destroy();
-                        },
-                        reset: function() {
-
-                            this.$destroy();
-                        }
+                this.$store.commit('modals/show', {
+                    name: 'pages-sources-dialog',
+                    context: { type: 'create' },
+                    original: source,
+                    events: {
+                        submit: (current) => { this.$store.commit('designer/sources/create', current) },
                     }
-                }).$mount()
+                })
             },
 
             update: function(source) {
 
-                new Shell.Sources.ModalEditor({
-
-                    data: {
-                        owner: this,
-                        original: source,
-                        current: JSON.parse(JSON.stringify(source))
-                    },
-
-                    methods: {
-                        submit: function() {
-
-                            this.owner.$emit('sources/update', {
-                                value: this.current,
-                                oldValue: this.original,
-                            });
-
-                            this.$destroy();
-                        },
-                        reset: function() {
-
-                            this.$destroy();
-                        }
+                this.$store.commit('modals/show', {
+                    name: 'pages-sources-dialog',
+                    context: { type: 'update' },
+                    original: source,
+                    events: {
+                        submit: (current) => { this.$store.commit('designer/sources/update', current) },
                     }
-                }).$mount()
+                })
             },
         }
     });
