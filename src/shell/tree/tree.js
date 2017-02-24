7a3921bbc51ea0bda931055c6f02aad33a3fc8c3
Igor Pavlenko <ipavlenko@inbox.ru>
2017-02-24 23:02:11 +0500

    Actions in tree panel

--- src/shell/tree/tree.js
+++ src/shell/tree/tree.js
@@ -2,12 +2,22 @@
 
     Vue.component('shell-tree', {
         template: '#shell-tree',
+        data: function() {
+            return {
+                widget: this.widget
+            }
+        },
+        created() {
+            this.widget = this.$store.state.designer.page.root
+        }
     });
 
     Vue.component('shell-tree-item', {
         template: '#shell-tree-item',
         props: {
-            widget: Object
+            parent: Object,
+            widget: Object,
+            level: Number,
         },
         data: function() {
             return {
@@ -15,7 +25,25 @@
             }
         },
         created() {
-            this.open = false
+            this.open = this.level < 3
+        },
+        methods: {
+            remove() {
+                this.$store.commit('designer/widgets/remove', { parent: this.parent, widget: this.widget });
+            },
+            update() {
+                this.$store.commit('modals/editor/show', {
+                    name: 'shell-widgets-dialog',
+                    context: {
+                        type: 'update',
+                        widget: this.$store.getters.palette.widget(this.widget.name)
+                    },
+                    original: this.widget,
+                    events: {
+                        submit: (current) => { this.$store.commit('designer/params/update', { model: this.widget, value: current }) },
+                    }
+                })
+            }
         }
     });
 
